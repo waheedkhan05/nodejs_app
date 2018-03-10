61050a2458eaf1106ff36ff7c1d50caa02c067dd
Waheed Khan <waheedkhan05@gmail.com>
2018-03-11 00:17:06 +0400

    getting rid of setTimeout, code cleanup for task1, making the results appear as required as web_address : title
    	modified:   controllers/task1.js
    	modified:   routes/index.js
    	modified:   views/index/index.pug

--- controllers/task1.js
+++ controllers/task1.js
@@ -4,50 +4,36 @@ var normalizeUrl = require('normalize-url');
 
 // We are facing callback hell, depending on the time it takes to load and parse the html from each link
 // the callbacks run at different times. To make sure our response is rendered with complete list of url 
-// titles keeping in view not to use any abstraction I have added setTimeout which is definitely 
-// not the way to do.
+// titles I am comparing addresses and titles. When they are the same I am rendering.
 
-const get =  function(req,resp,title_array){
-            if (req.query.address) {
-            let addresses = "";
-                if (req.query.address instanceof Array) {
-                    addresses = req.query.address;
-                } else {
-                    addresses = [req.query.address];
-                }
+const get =  function(addresses, titles, callback){
                 addresses.forEach(element => {
-                    scrapeTitle(normalizeUrl(element),title_array,resp);
+                    scrapeTitle(normalizeUrl(element),titles,callback);
                 });
-            }
     }
 
 
-
-function scrapeTitle(urlToScrape,title_array,res){
+function scrapeTitle(urlToScrape,titles,callback){
     request({
         method: 'GET',
         url: urlToScrape
     }, function(err, response, body) {
         // in case of error, return the error
-        if (err) { title_array.push(err); return console.error(err);}
+        if (err) { titles.push(err.message); return console.error(err.message);}
         // Tell Cheerio to load the HTML page for scraping the title
         try{
             $ = cheerio.load(body);
             const scrapedTitle = $('head > title').text().toString();
-            title_array.push(scrapedTitle);
+            titles.push(scrapedTitle);
             console.log(scrapedTitle);
+            callback();
         }
         catch(error){
-            title_array.push({urlToScrape : error.message});
-            console.log(error.message);
+            console.error(error.message);
+            titles.push(error);
+            callback();
         }
     });
-    // render response, the titles of the addresses query.
-    setTimeout(function(){
-        res.render('index/index', {
-        title: " Following are the titles of given websites:",
-        scrapedTitles:  title_array
-      }); }, 2500);
 }
 
 module.exports.usingCallback = get;
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,27 @@ var router = express.Router();
 
 // GET: /
 router.get('/I/want/title', function(req, res) {
-  let url_titles = [];
-  task2.usingAsync(req,res);
+  let titles = [];
+  let addresses = [];
+  addresses = req.query.address
+  if (!addresses instanceof Array) {
+	    addresses = [addresses];
+  }
+  task1.usingCallback(addresses,titles,function(){
+    // comparing titles and addresses to make sure all the functions have run and returned the response.
+    if (titles.length == addresses.length){
+      // transforming results in the required format
+      let results = {};
+      for (let i = 0; i < titles.length; i++) {
+        results[addresses[i]] = titles[i];
+      }
+      console.log(results);
+      res.render('index/index', {
+          title: " Following are the titles of given websites:",
+          scrapedTitles:  results
+      });
+    }
+  });
 });
 // Get for any other route besides I/want/title
 router.get('/*', function(req, res){
