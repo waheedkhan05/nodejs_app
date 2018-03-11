2cd0a570b4cfdc711b704ba38eaf64f192c4a09a
Waheed Khan <waheedkhan05@gmail.com>
2018-03-11 11:07:55 +0400

    Adding some changes, code refactoring. setting up a valid head title.
     Changes to be committed:
    	modified:   controllers/task2.js
    	modified:   views/shared/head.pug

--- controllers/task2.js
+++ controllers/task2.js
@@ -3,43 +3,78 @@ const request = require("request");
 const cheerio = require('cheerio');
 const normalizeUrl = require('normalize-url');
 
-var task2 = function(req,resp){
-    let title_array = [];
-    let addresses = "";
-    if (req.query.address) {
-            if (req.query.address instanceof Array) {
-                addresses = req.query.address;
-            } else {
-                addresses = [req.query.address];
+var task2 = function(req,res,addresses,callback){
+    // let titles = [];
+    // let addresses = [];
+    // if (req.query.address) {
+    //         if (req.query.address instanceof Array) {
+    //             addresses = req.query.address;
+    //         } else {
+    //             addresses = [req.query.address];
+    //         }
+    //     }
+    //     async.filter(addresses, function(address, callback) {
+            
+    //         request({
+    //             method: 'GET',
+    //             url: normalizeUrl(address)
+    //         }, function(err, response, body) {
+    //             // in case of error, return the error
+    //             if (err) { titles.push(err.message); return callback(err,titles);}
+    //             // Tell Cheerio to load the HTML page for scraping the title
+    //             try{
+    //             $ = cheerio.load(body);
+    //             const scrapedTitle = $('head > title').text().toString();
+    //             titles.push(scrapedTitle);
+    //             return callback(err,titles);
+    //             }
+    //             catch(error){
+    //                 callback(error,titles);
+    //             }
+               
+    //         });
+    //       }, function(err, results) {
+    //         if (err) { titles.push(err);  console.error(err);}
+    //           console.log(results);
+    //           resp.render('index/index', {
+    //             title: " Following are the titles of given websites:",
+    //             scrapedTitles:  titles
+    //           });
+    //       });
+    let stack = [];
+    let titles = [];
+    addresses.forEach(element => {
+        stack.push(
+            function(callback){
+                return function(){
+                    request({
+                        method: 'GET',
+                        url: normalizeUrl(element)
+                    }, function(err, response, body) {
+                        // in case of error, return the error
+                        // if (err) { titles.push(err.message); return callback(err,titles);}
+                        // Tell Cheerio to load the HTML page for scraping the title
+                        $ = cheerio.load(body);
+                        const scrapedTitle = $('head > title').text().toString();
+                        return callback(err,scrapedTitle);
+                });
+                return callback(err,scrapedTitle);
             }
-        }
-        async.filter(addresses, function(address, callback) {
-            request({
-                method: 'GET',
-                url: normalizeUrl(address)
-            }, function(err, response, body) {
-                // in case of error, return the error
-                if (err) { title_array.push(err); return callback(err);}
-                // Tell Cheerio to load the HTML page for scraping the title
-                try{
-                    $ = cheerio.load(body);
-                    const scrapedTitle = $('head > title').text().toString();
-                    title_array.push(scrapedTitle);
-                    callback(err,title_array);
+    });
+});
+    async.parallel(stack,
+    function(err, results) {
+        if(err){console.error(err.message);}
+        let output = {};
+        console.log(results);
+        for (let i = 0; i < results.length; i++) {
+                  output[addresses[i]] = results[i];
                 }
-                catch(error){
-                    title_array.push({urlToScrape : error.message});
-                    callback(error.message,title_array);
-                }
-            });
-          }, function(err, results) {
-            if (err) { title_array.push(err); return console.error(err);}
-            //   console.log(title_array);
-              resp.render('index/index', {
-                title: " Following are the titles of given websites:",
-                scrapedTitles:  title_array
-              });
-          });
+        res.render('index/index', {
+                    title: " Following are the titles of given websites:",
+                    scrapedTitles:  output
+                });
+    });
 }
 
 module.exports.usingAsync = task2;
